cb237ead93f1f13ad64f0f628b78370070766579
Evelyn Francisco <evelynbrandao15@gmail.com>
2020-03-27 19:30:56 -0300

    Evelyn: Criação da função generica para criar tabela. Falta efetuar as testes para ver se funciona.

--- website/global/Interface.php
+++ website/global/Interface.php
@@ -5,7 +5,27 @@ namespace website\classe;
 // interface IGlobal{
 trait IGlobal{
 
-    public function rederizarHTML(){
-        include "website//login//$this->html";
+
+    public function rederizarTabela($thead=[], $tbody=[]){
+        $htmlTabela =" <thead><tr>";
+        foreach($thead as $cabecalho){
+            $htmlTabela .="<th>$cabecalho</th>";
+        }
+        $htmlTabela .="</tr></thead>";
+        $htmlTabela .="<tbody>";
+        foreach($tbody as $linha){
+            $htmlTabela .="<tr>";
+            foreach($linha as $coluna){
+                $htmlTabela .="<td>$coluna</td>";
+            }
+            $htmlTabela .="</tr>";
+        }
+        $htmlTabela .="</tbody>";
+
+        return $htmlTabela;
+    }
+
+    public function rederizarHTML($pasta, $html){
+        include "website//$pasta//$html";
     }
 }
