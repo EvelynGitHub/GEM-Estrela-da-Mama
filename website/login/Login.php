1f76ef844c84892bc4c32e921afacb407f98044f
GiovanaNapoli <32879408+GiovanaNapoli@users.noreply.github.com>
2020-04-01 17:43:55 -0300

    Login.php login.html

--- website/login/Login.php
+++ website/login/Login.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+
 
 namespace website\classe;
 
@@ -15,50 +15,57 @@ class Login {
     private $usuario;
     private $senha;
 
-    public function __contructor(){
+    public function __contructor($usuario, $senha){
         $this->usuario=$usuario;
         $this->senha=$senha;
+
+        $this->Login();
     }
 
-    public function verificaLogin(){
+    /*public function verificaLogin(){
         //verifica se há uma sessão ativa
         //se não tiver retorna para pagina de login
-        if(isset($_POST['usuario'] && isset($_POST['senha']))){
-            header('Location: login.html');
-            exit();
+        if(isset($_POST['usuario']) && isset($_POST['senha'])){
+            header('Location: /login.html');
+            //exit();
         }
-    }
+    }*/
 
     public function Login(){
+
+        session_start();
         
         //$usuario = mysqli_real_escape_string($conn, $_POST['usuario']);
         //$senha = mysqli_real_escape_string($conn, $_POST['senha']);
 
-        $sql = "SELECT * FROM login nm_login = :usuario, nm_senha = :senha";
+        $sql = "SELECT * 
+                FROM login 
+                WHERE nm_login = :usuario 
+                AND nm_senha = :senha";
 
-        $preparaSql = array(':nm_login' => $usuario, ':senha' => $senha);
+        $preparaSql = array(':usuario' => $this->$usuario, ':senha' => $this->$senha);
 
         $banco = new CRUD();
         $matriz = $banco->obterRegistros($sql, $preparaSql);
 
-
         //não entendi o CRUD direito, mas se for retornado 1 registro, faz a sessão
         //muito provavelmente oq eu fiz esta errado kk
-        if($matriz == 1){
-            $_SESSION['usuario'] = $usuario;
-            header('Location: ');
-            exit();
+
+        if(count($matriz) == 1){
+            $_SESSION['usuario'] = $this->$usuario;
+            header('Location: /lista-geral.html');
+            //exit();
         }else{
             $_SESSION['nao_autenticado'] = "Nome de usuário ou senha invalidos";
-            header('Location: login.html');
-            exit();
+            header('Location: /login.html');
+            //exit();
         }
     }
 
     public function Lougout(){
         //destruir sessão
         session_destroy();
-        header('Location: login.html');
-        exit();
+        header('Location: /login.html');
+        //exit();
     }
 }
