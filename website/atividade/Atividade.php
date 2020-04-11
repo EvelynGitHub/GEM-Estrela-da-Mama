64dd8d5c9697ead25634412b4373d17a2e96b199
eric-araujo <eric.araujolima@hotmail.com>
2020-04-11 02:52:53 -0300

    Adicionado tratamento de campos nulos no cadastro de atividade

--- website/atividade/Atividade.php
+++ website/atividade/Atividade.php
@@ -2,14 +2,16 @@
 
 namespace website\classe;
 
-require_once __DIR__.'/../global/Interface.php';
-require_once __DIR__.'/../global/CRUD.php';
+require_once __DIR__ . '/../global/Interface.php';
+require_once __DIR__ . '/../global/CRUD.php';
 
 use Exception;
 use CRUD;
-class Atividade {
 
-	use IGlobal;
+class Atividade
+{
+
+    use IGlobal;
 
     private $nomeAtividade;
     private $horaAtividade;
@@ -17,83 +19,93 @@ class Atividade {
     private $afiliado;
     private $quantidadeAulas;
     private $tabela = 'atividade';
-	
-	public function __construct($nomeAtividade = "", $horaAtividade = "", $date = "", $afiliado = "", $quantidadeAulas = ""){
 
-        
-		$this->nomeAtividade = $nomeAtividade;
-		$this->horaAtividade = $horaAtividade;
-		$this->date = $date;
-    	$this->afiliado = $afiliado;
-		$this->quantidadeAulas = $quantidadeAulas;
+    public function __construct($nomeAtividade = "", $horaAtividade = "", $date = "", $afiliado = "", $quantidadeAulas = "")
+    {
 
-	}
 
-	public function __get($atributo){
-        if(!property_exists($this, $atributo)){
-            throw new Exception("Atributo {$atributo} não existe nessa classe");
-		}
-		
-      	return $this->{$atributo};
+        $this->nomeAtividade = $nomeAtividade;
+        $this->horaAtividade = $horaAtividade;
+        $this->date = $date;
+        $this->afiliado = $afiliado;
+        $this->quantidadeAulas = $quantidadeAulas;
     }
 
-    public function __set($atributo, $valor){
-     
-        if(!property_exists($this, $atributo)){
+    public function __get($atributo)
+    {
+        if (!property_exists($this, $atributo)) {
             throw new Exception("Atributo {$atributo} não existe nessa classe");
-		}
+        }
+
+        return $this->{$atributo};
+    }
+
+    public function __set($atributo, $valor)
+    {
+
+        if (!property_exists($this, $atributo)) {
+            throw new Exception("Atributo {$atributo} não existe nessa classe");
+        }
 
         $this->{$atributo} = $valor;
     }
 
-    public function criarAtividade($atividade){
-        $crud = new CRUD();
+    public function criarAtividade($atividade)
+    {
 
-        $arrayValores = array(
-                        'nm_atividade' => $atividade->nomeAtividade,
-                        'dt_atividade' => $atividade->date,
-                        'hr_hora' => $atividade->horaAtividade
-                        );
+        if ($this->verificarCamposNulosAtividade()) {
 
-        $crud->inserirGenerico($this->tabela, $arrayValores);
-    }
-    public function editarAtividade($atividade){
+            $crud = new CRUD();
 
-    }
-    public function excluirAtividade($atividade){
+            $arrayValores = array(
+                'nm_atividade' => $atividade->nomeAtividade,
+                'dt_atividade' => $atividade->date,
+                'hr_hora' => $atividade->horaAtividade
+            );
 
+            $crud->inserirGenerico($this->tabela, $arrayValores);
+        }else{
+            echo "Preencha todos os campos";
+        }
     }
-    public function listarAtividades(){
-
+    public function editarAtividade($atividade)
+    {
     }
-    public function adicionarAfiliado($afiliado){
-
+    public function excluirAtividade($atividade)
+    {
     }
-    public function removerAfiliado($afiliado){
-
+    public function listarAtividades()
+    {
     }
-    private function verificatAtividadeExiste($atividade){
-
+    public function adicionarAfiliado($afiliado)
+    {
     }
-    private function verificarCamposNulosAtividade(){
-        
+    public function removerAfiliado($afiliado)
+    {
     }
-    
+    private function verificatAtividadeExiste($atividade)
+    {
+    }
+    private function verificarCamposNulosAtividade()
+    {
+        if ($this->nomeAtividade == null && $this->horaAtividade == null && $this->date == null) {
+            return false;
+        }
+        return true;
+    }
 }
 
 if (isset($_POST['btn-cadastrar'])) {
 
     $atividade = new Atividade();
-    
-    try{
+
+    try {
         $atividade->nomeAtividade = $_POST['name'];
         $atividade->horaAtividade = $_POST['horario'];
         $atividade->date = $_POST['data'];
 
         $atividade->criarAtividade($atividade);
-
-    }catch(Exception $e){
+    } catch (Exception $e) {
         return $e->getMessage();
     }
-
 }
