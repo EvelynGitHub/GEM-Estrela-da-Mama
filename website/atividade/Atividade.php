e97e3db9e21214bb7bb4696c94672b4a5b2cd72c
GiovanaNapoli <32879408+GiovanaNapoli@users.noreply.github.com>
2020-04-13 20:02:25 -0300

    teste
    teste

--- website/atividade/Atividade.php
+++ website/atividade/Atividade.php
@@ -87,7 +87,7 @@ class Atividade
                 for($i = 0; $i <= count($matriz); $i++)
                 {
                     $pegaValores['nm_atividade'] = mysqli_fetch_assoc($matriz[$i]);
-                    echo  "<button class = 'ver-atividade'>$pegaValores </button>";
+                    echo  "<button class = 'ver-atividade'>$pegaValores['nm_atividade'] </button>";
                 
                 }
                 //echo rederizarTabela($matriz );
