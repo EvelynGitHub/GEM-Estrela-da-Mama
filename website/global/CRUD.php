339f93d8f8086d485cf16fc6b9a4eefe443e89fd
GiovanaNapoli <32879408+GiovanaNapoli@users.noreply.github.com>
2020-05-03 15:34:53 -0300

    TesteCad
    testee

--- website/global/CRUD.php
+++ website/global/CRUD.php
@@ -141,7 +141,7 @@ class CRUD extends Conexao
             if ($instrucao->execute()) {
                 return "<script>
                             alert('Cadastro realizado com sucesso!');
-                            //self.location.href='/afiliado/cadastrar';
+                           
                         </script>";
             } else {
                 return "<script>
@@ -154,7 +154,7 @@ class CRUD extends Conexao
             exit;
         }
     }
-
+ //self.location.href='/afiliado/cadastrar';
     public function updateGenerico($tabela, $valores, $condicaoArray)
     {
         try {
