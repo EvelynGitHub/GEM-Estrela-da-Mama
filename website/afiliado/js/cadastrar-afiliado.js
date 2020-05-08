d2e0b0f0bbe836d182668bfe6bf2058d6e0eab28
eric-araujo <eric.araujolima@hotmail.com>
2020-05-08 15:19:47 -0300

    Ĺógica do botão voltar alterada

--- website/afiliado/js/cadastrar-afiliado.js
+++ website/afiliado/js/cadastrar-afiliado.js
@@ -1,20 +1,36 @@
 let radioButtonTipoAssistida = document.querySelector("#tipoAssistida");
 let radioButtonTipoVoluntario = document.querySelector("#tipoVoluntario");
+
 let divVoluntario = document.querySelector("#voluntario");
 let divAssistida = document.querySelector("#assistida");
+
 let inputInteresseVoluntario = document.querySelector("#interesse");
 
+let btnCancelar = document.querySelector("#btn-cancelar");
+
 radioButtonTipoAssistida.addEventListener('click', () => {
+
     divVoluntario.setAttribute("hidden", "hidden");
     divAssistida.removeAttribute("hidden");
 
     inputInteresseVoluntario.removeAttribute("required");
+
 });
 
 radioButtonTipoVoluntario.addEventListener('click', () => {
+
     divAssistida.setAttribute("hidden", "hidden");
     divVoluntario.removeAttribute("hidden");
 
     inputInteresseVoluntario.setAttribute("required", "required");
+
 });
+
+btnCancelar.addEventListener('click', () => {
+
+    window.location.pathname = '/lista-geral';
+
+});
+
+
 
