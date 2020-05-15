d7da6fb05f3fb6007bd0d5b15cece130803fbce9
EVELYN FRANCISCO BRANDAO <evelyn.brandao@fatec.sp.gov.br>
2020-05-14 23:56:24 -0300

    Evelyn: criação da função validarCPF e removerLetras

--- website/afiliado/js/afiliado.js
+++ website/afiliado/js/afiliado.js
@@ -5,21 +5,29 @@ let celular = document.getElementById("celular")
 
 // Deste modo os valores são obtidos enquanto digitados
 cpf.addEventListener("input", ({ target }) => {
+    removeLetras(target)
     removePontuacao(target)
-    mascaraInput(target, "000.000.000-00")
+    if (validarCPF(target)) {
+        mascaraInput(target, "000.000.000-00")
+    }
+    // habilitar e desabilitar botão  cadastrar e editar dependendo do if
+    // Colocar a cor do input vermelha ou verde ???
 })
 
 telefone.addEventListener("input", ({ target }) => {
+    removeLetras(target)
     removePontuacao(target)
     mascaraInput(target, "(00)0000-0000")
 })
 
 celular.addEventListener("input", ({ target }) => {
+    removeLetras(target)
     removePontuacao(target)
     mascaraInput(target, "(00)00000-0000")
 })
 
 cep.addEventListener("input", ({ target }) => {
+    removeLetras(target)
     removePontuacao(target)
     mascaraInput(target, "00000-000")
 })
@@ -53,6 +61,38 @@ function removePontuacao(input) {
     input.value = textoSemParenteses;
 }
 
+function removeLetras(input) {
+    let texto = input.value
+    input.value = texto.replace(/[^\d]+/g, '')
+}
+
+function validarCPF(input) {
+    let valor = input.value.split('')
+    let soma = 0
+    let cont = 10
+    
+    for (let index = 0; index < valor.length - 2; index++) {
+        soma = soma + (valor[index] * cont--);
+    }
+
+    let digito1 = ((11 - (soma % 11)) >= 10) ? 0 : (11 - (soma % 11))
+
+    soma = 0
+    cont = 11
+    for (let index = 0; index < valor.length - 1; index++) {
+        soma = soma + (valor[index] * cont--);
+    }
+
+    let digito2 = ((11 - (soma % 11)) >= 10) ? 0 : (11 - (soma % 11))
+    
+    if (valor[9] == digito1 && valor[10] == digito2) {
+        // console.log("Este CPF é válido")
+        return true
+    } else {
+        // console.log("Este CPF NÃO é válido")
+        return false
+    }
+}
 
 function inicializarEditar() {
     mascaraInput(cpf, "000.000.000-00")
