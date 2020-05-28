ba29e93a7b840fde4f2801ea4ae95c9b6e1122c1
eric-araujo <eric.araujolima@hotmail.com>
2020-05-28 19:10:24 -0300

    Exibição de frequencia afiliado formatada

--- website/Frequencia/Frequencia.php
+++ website/Frequencia/Frequencia.php
@@ -36,9 +36,8 @@ class Frequencia
     {
 
         try {
-            //faço um select da tabela chamada
             $sql = "SELECT a.nm_afiliado 'Nome', a.nm_tipo_afiliado 'Tipo', c.qt_presencas 'Presença', c.qt_faltas 'Faltas',
-                    ((c.qt_presencas)/((c.qt_presencas + c.qt_faltas)*100)) AS Frequencia
+                    CONCAT(FORMAT((((c.qt_presencas)/(c.qt_presencas + c.qt_faltas))*100), 2), '%') AS 'Frequência'
                     FROM afiliado a, chamada c
                     WHERE c.id_afiliado = a.cd_afiliado
                     ORDER BY ':order'";
@@ -47,9 +46,6 @@ class Frequencia
 
             $banco = new CRUD();
             $matriz = $banco->obterRegistros($sql, $preparaSql);
-
-            //$banco = new CRUD();
-            //$matriz = $banco->obterRegistros($sql);
 
             echo $this->rederizarTabela($matriz);
         } catch (Exception $e) {
