cfd526802cfb1ce00bf732b0ba2562a6f13b6e35
GiovanaNapoli <32879408+GiovanaNapoli@users.noreply.github.com>
2020-05-31 18:19:33 -0300

    Busca por nome
    precisa de correção, mas ta funcional

--- website/administrador/Administrador.php
+++ website/administrador/Administrador.php
@@ -7,106 +7,110 @@ require_once __DIR__ . '/../global/CRUD.php';
 
 use Exception;
 use CRUD;
-class Administrador {
+
+class Administrador
+{
 	use IGlobal;
 
-    private $usuario;
+	private $usuario;
 	private $senha;
-	
-	public function __construct($usuario = "", $senha= ""){
+
+	public function __construct($usuario = "", $senha = "")
+	{
 
 		$this->usuario = $usuario;
-		$this->senha = $senha;	
-
+		$this->senha = $senha;
 	}
 
-	public function __get($atributo){
-        if(!property_exists($this, $atributo)){
-            throw new Exception("Atributo {$atributo} não existe nessa classe");
+	public function __get($atributo)
+	{
+		if (!property_exists($this, $atributo)) {
+			throw new Exception("Atributo {$atributo} não existe nessa classe");
 		}
-		
-      	return $this->{$atributo};
-    }
 
-    public function __set($atributo, $valor){
-     
-        if(!property_exists($this, $atributo)){
-            throw new Exception("Atributo {$atributo} não existe nessa classe");
-        }
+		return $this->{$atributo};
+	}
 
-        $this->{$atributo} = $valor;
-    }
-    
-    public function AdicionarAdminstrador($usuario,$senha,$conrfimarSenha){
-		if($senha == $conrfimarSenha){
-		$crud = new Crud();            
+	public function __set($atributo, $valor)
+	{
 
-		$sql = "SELECT nm_login FROM  login
+		if (!property_exists($this, $atributo)) {
+			throw new Exception("Atributo {$atributo} não existe nessa classe");
+		}
+
+		$this->{$atributo} = $valor;
+	}
+
+	public function AdicionarAdminstrador($usuario, $senha, $conrfimarSenha)
+	{
+		if ($senha == $conrfimarSenha) {
+			$crud = new Crud();
+
+			$sql = "SELECT nm_login FROM  login
 				WHERE  nm_login = :usuario";
-			
-		$preparaSql = array(":usuario" => $usuario);
-		//var_dump($preparaSql);
-		$matriz = $crud->obterRegistros($sql, $preparaSql);
-		
-		//var_dump($matriz);
-		
-		if(empty($matriz)){	
-			//echo "entrou";
 
-			$addAdministrador = array(
-				'nm_login' => $usuario,
-				'nm_senha'=> $senha 
-			);
-			//var_dump($addAdministrador);
+			$preparaSql = array(":usuario" => $usuario);
+			//var_dump($preparaSql);
+			$matriz = $crud->obterRegistros($sql, $preparaSql);
 
-			$retorno = $crud->inserirGenerico("login",$addAdministrador);
-			//var_dump($retorno);
+			//var_dump($matriz);
 
-			
-			echo "<script>
+			if (empty($matriz)) {
+				//echo "entrou";
+
+				$addAdministrador = array(
+					'nm_login' => $usuario,
+					'nm_senha' => $senha
+				);
+				//var_dump($addAdministrador);
+
+				$retorno = $crud->inserirGenerico("login", $addAdministrador);
+				//var_dump($retorno);
+
+
+				echo "<script>
 					alert('Cadastrado com sucesso');
 					self.location.href='/lista-geral';					
 				</script>";
-				
-				die();	
-			//header('Location: /lista-geral');
-			
-			//header('administrador/teste.html');
-		}else{
-			echo "<script>
+
+				die();
+				//header('Location: /lista-geral');
+
+				//header('administrador/teste.html');
+			} else {
+				echo "<script>
 					alert('email ja cadastrado');	
 					self.location.href='/lista-geral';	
 				</script>";
 				die();
-		}
-	
-	}else{
-		echo "<script> alert('as senhas não correspondem');
+			}
+		} else {
+			echo "<script> alert('as senhas não correspondem');
 					   self.location.href='cadastrar-administrador';
 		</script>";
+		}
 	}
+	public function editarAdminstrador($administrador)
+	{
 	}
-	public function editarAdminstrador($administrador){
-
+	public function removerAdminstrador($administrador)
+	{
 	}
-	public function removerAdminstrador($administrador){
-
+	public function listarAdministradores()
+	{
 	}
-	public function listarAdministradores(){
-
+	public function fazerusuario($administrador)
+	{
 	}
-	public function fazerusuario($administrador){
-
-    }
 }
 
 $user = new Administrador();
 
 $login = isset($_POST["novousuario"]) ? $_POST["novousuario"] : "";
 $senha1 = isset($_POST["novasenha"]) ? $_POST["novasenha"] : "";
-$conrfimarSenha=isset($_POST["confirmarsenha"]) ? $_POST["confirmarsenha"] : "";
+$conrfimarSenha = isset($_POST["confirmarsenha"]) ? $_POST["confirmarsenha"] : "";
 
 if (isset($_POST["cadastrarAdm"])) {
-    $user->AdicionarAdminstrador($login, $senha1,$conrfimarSenha);
-    unset($_POST["cadastrarAdm"]);
+	$user->AdicionarAdminstrador($login, $senha1, $conrfimarSenha);
+	unset($_POST["cadastrarAdm"]);
 }
