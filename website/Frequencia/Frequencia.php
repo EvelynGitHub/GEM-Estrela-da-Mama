1a983a5e802f25d479c44617d9a211dbcc7b8541
viits <31901624+viits@users.noreply.github.com>
2020-06-04 17:37:17 -0300

    Check Box
    Frequencia

--- website/Frequencia/Frequencia.php
+++ website/Frequencia/Frequencia.php
@@ -36,38 +36,37 @@ class Frequencia
     {
 
         try {
-            $sql = "SELECT a.nm_afiliado 'Nome', a.nm_tipo_afiliado 'Tipo', c.qt_presencas 'Presença', c.qt_faltas 'Faltas',
-                    CONCAT(FORMAT((((c.qt_presencas)/(c.qt_presencas + c.qt_faltas))*100), 2), '%') AS 'Frequência'
-                    FROM afiliado a
-                    JOIN chamada c ON (a.cd_afiliado = c.id_afiliado) ";
-            //var_dump("<h6>Verficando o select antes do if</h6> ",$sql, "<br><br>");
+            $sql = "SELECT a.cd_afiliado '#', a.nm_afiliado 'Nome', a.nm_tipo_afiliado 'Tipo', c.qt_presencas 'Presença',
+            CONCAT(FORMAT((((c.qt_presencas)/365)*100), 2), '%') AS 'Frequência', null 'Marcar'
+            FROM afiliado a
+            JOIN chamada c ON (a.cd_afiliado = c.id_afiliado) ";
+    //var_dump("<h6>Verficando o select antes do if</h6> ",$sql, "<br><br>");
 
-            $preparaSQL = "";
-            //var_dump("<h6>Verficando o preparaSQL antes do if</h6> ",$preparaSQL, "<br><br>");
+    $preparaSQL = "";
+    //var_dump("<h6>Verficando o preparaSQL antes do if</h6> ",$preparaSQL, "<br><br>");
 
-            $banco = new CRUD();
+    $banco = new CRUD();
 
-            if (isset($_GET['busca'])) {
+    if (isset($_GET['busca'])) {
 
-                $nome = isset($_GET['pesquisa']) ? $_GET['pesquisa'] : "";
-                //var_dump("<h6>Verficando a varivael nome dentro do if</h6> ",$nome, "<br><br>");
+        $nome = isset($_GET['pesquisa']) ? $_GET['pesquisa'] : "";
+        //var_dump("<h6>Verficando a varivael nome dentro do if</h6> ",$nome, "<br><br>");
 
-                if ($nome != "") {
+        if ($nome != "") {
 
-                    $sql .= "WHERE a.nm_afiliado LIKE :nome";
-                    //var_dump("<h6>Verficando o select dentro do if</h6> ",$sql, "<br><br>");
+            $sql .= "WHERE a.nm_afiliado LIKE :nome";
+            //var_dump("<h6>Verficando o select dentro do if</h6> ",$sql, "<br><br>");
 
-                    $preparaSQL = array(':nome' => "%$nome%");
-                    //var_dump("<h6>Verficando o preparaSQL dentro do if</h6> ",$preparaSQL, "<br><br>");
-                }
-            }
-            
+            $preparaSQL = array(':nome' => "%$nome%");
+            //var_dump("<h6>Verficando o preparaSQL dentro do if</h6> ",$preparaSQL, "<br><br>");
+        }
+    }
 
-            $matriz = $banco->obterRegistros($sql, $preparaSQL);
-            //var_dump("<h6>Verficando o que a matriz esta passando</h6> ",$matriz, "<br>");
-            
-            $htmlPresente['#'] = array('Presença' => "<input class='chk' type='checkbox' name='afiliado[]' value='@codigo@'>");
-            echo $this->rederizarTabela($matriz,$htmlPresente,"@codigo@");
+    $matriz = $banco->obterRegistros($sql, $preparaSQL);
+    //var_dump("<h6>Verficando o que a matriz esta passando</h6> ",$matriz, "<br>");
+
+    $htmlPresente['#'] = array('Marcar' => "<input class='chk' type='checkbox' name='afiliado[]' value='@codigo@'>");
+    echo $this->rederizarTabela($matriz, $htmlPresente, "@codigo@");
 
         } catch (Exception $e) {
             echo "Erro ao listar a presença dos afiliados:" . $e;
