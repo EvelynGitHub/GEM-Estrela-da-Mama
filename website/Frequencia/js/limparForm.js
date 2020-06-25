4c29834997f1499bb732d6396f7825c2f93b66bd
Evelyn <evelynbrandao15@gmail.com>
2020-06-25 14:00:11 -0300

    Evelyn: precistir presença depois de sair de visualizar-frequencia

--- website/Frequencia/js/limparForm.js
+++ website/Frequencia/js/limparForm.js
@@ -1,11 +1,40 @@
-window.onload = () => {
+// window.onload = () => {
 
-    let form = document.querySelector("#frequencia");
+//     let form = document.querySelector("#frequencia");
 
-    if (form) {
+//     if (form) {
 
-        form.reset();
+//         form.reset();
 
+//     }
+
+// }
+
+
+$(document).ready(function () {
+
+    //var id = sessionStorage.getItem("listaAfiliados").split(",");
+    let listaAfiliados = sessionStorage.getItem("listaAfiliados").split(",");
+
+    for (let x = 0; x < listaAfiliados.length; x++) {
+        $("input[value='" + listaAfiliados[x] + "']").prop("checked", true);
     }
 
-}
+
+    $("input[name*='afiliado']").click(function () {
+
+        if (listaAfiliados.includes(this.value)) {
+            listaAfiliados.splice(listaAfiliados.indexOf(this.value), 1)
+        } else {
+            listaAfiliados.push(this.value)
+        }
+        sessionStorage.setItem("listaAfiliados", listaAfiliados);
+
+    });
+
+
+
+
+
+
+});
